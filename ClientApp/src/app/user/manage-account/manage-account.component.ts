3b31c06a463856993d7022126a6f46d0e42237dd
AtinaF <atina97_folic@uns.ac.rs>
2023-11-15 18:34:52 +0100

    initial Angular application
    starting point for ClientSide project

---
+++ ClientApp/src/app/user/manage-account/manage-account.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-manage-account',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './manage-account.component.html',
+  styleUrl: './manage-account.component.css'
+})
+export class ManageAccountComponent {
+
+}
